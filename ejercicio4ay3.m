766959a06701a0e7e67c2b7b075f1d3c6a0d1f25
jael716 <37382089+jael716@users.noreply.github.com>
2018-04-25 20:13:38 +0000

    Delete ejercicio4ay3.m

--- ejercicio4ay3.m
+++
@@ -1,7 +0,0 @@
-w = [-6 : 0.01 : 6] * pi;
-X = exp(j*w) ./ (exp(j*w) - 0.9);
-
-subplot 211; plot(w/pi, abs(X)); grid
-xlabel('w / Pi [rad/m]'); title('Magnitud')
-subplot 212; plot(w/pi, unwrap(angle(X))/pi); grid
-xlabel('w / Pi [rad/m]'); title('Angulo / Pi [rad]')
